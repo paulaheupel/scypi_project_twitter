52233be2a4464314b8cd645f06923467d7578bc8
paulaheupel <35920199+paulaheupel@users.noreply.github.com>
2022-07-10 12:15:46 +0200

    loading, cleaning data implemented

--- load_data.py
+++ load_data.py
@@ -1,10 +1,15 @@
 import pandas as pd
+import numpy as np
 
 def load_data_to_df(data_path):
     tweets_df = pd.read_csv(data_path)
-    return tweets_df
+    tidy_tweets = tidy_up_data(tweets_df)
+    return tidy_tweets
 
-#def tidy_up_data(df):
+def tidy_up_data(df):
+    clean_df = df.dropna(how = "any", axis = 1)
+    clean_df = clean_df.drop(["language", "id"], axis = 1)
+    return clean_df
 
 
 if __name__ == "__main__":
